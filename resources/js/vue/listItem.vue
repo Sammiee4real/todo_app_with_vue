f849be74edb0fddea02bf805b7c97f214c94c6d3
Sammiee4real <adebsholey4real@gmail.com>
2021-03-18 14:53:41 +0100

    laravel with vue todo app first commit

---
+++ resources/js/vue/listItem.vue
@@ -0,0 +1,71 @@
+<template>
+     <!-- this handles a single request per time -->
+         <div class="item">
+             <input type="checkbox" 
+                @change="updateCheckbox()"
+                v-model="item.completed"
+             />
+             <span :class="[item.completed ? 'completed': '','itemText']">{{ item.name }}</span>
+             <button @click="removeItem()" class="trashcan">
+                 <font-awesome-icon icon='trash' />
+             </button>
+         </div>
+  
+</template>
+
+<script>
+    export default{
+        props:['item'],
+        methods:{
+           updateCheckbox(){
+                axios.put('api/item/' + this.item.id,{
+                  item: this.item
+                })
+                .then( response=>{
+                    if(response.status == 200){
+                        this.$emit('itemchanged');
+                    }
+                })
+                .catch( error => {
+                    console.log(error);
+                })
+           },
+           removeItem(){
+               axios.delete('api/item/' + this.item.id)
+               .then( response => {
+                   if( response.status == 200){
+                       this.$emit('itemchanged');
+                   }
+               } )
+               .catch( error => {
+                   console.log(error)
+               } )
+           }
+
+        }
+    }
+   
+</script>
+
+<style scoped>
+  .completed{
+      text-decoration: line-through;
+      color: #999999;
+  }
+  .itemText{
+      width: 100%;
+      margin-left:20px;
+  }
+  .item{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+  }
+  .trashcan{
+      background: #e6e6e6;
+      color: #ff0000;
+      outline:none;
+      border: none;
+  }
+</style>
+
